ab0f4e92633f7fa3bd631c1b171add1e77e45b1e
Arathi v r <arathi.vr@simelabs.com>
2021-11-08 13:01:29 +0530

    java

---
+++ Arrays/src/Main.java
@@ -0,0 +1,25 @@
+import java.math.BigDecimal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] marks = {97,98,100};
+        Student student = new Student("Ram",marks);
+
+        int number = student.getNumberOfMarks();
+        System.out.println("number of marks: " + number);
+
+        int sum = student.getTotalSumOfMarks();
+        System.out.println("Sum of marks: " + sum);
+
+        int maximumMark = student.getMaximumMark();
+        System.out.println("Maximum mark : " + maximumMark);
+
+        int minimumMark = student.getMinimumMark();
+        System.out.println("Minimum mark: " + minimumMark);
+
+        BigDecimal average = student.getAverageMarks();
+        System.out.println("Average mark: " + average);
+
+    }
+}
